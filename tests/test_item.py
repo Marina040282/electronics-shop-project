c49d58e519fefebafe4b4cecc584ec55f2af973d
Marina040282 <119851129+Marina040282@users.noreply.github.com>
2023-05-10 22:43:00 +0300

    1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from electronics_shop_project.src.item import Item
+
+def test_calculate_total_price():
+    assert Item.calculate_total_price(20000, 5) == 10000
+
+
+def test_apply_discount():
+    assert Item.apply_discount(0.8, 10000) == 8000
