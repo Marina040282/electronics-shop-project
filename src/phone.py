c9eecc6ab63f0661ba4b5fb6e2fac7dbf1117ef5
Marina040282 <119851129+Marina040282@users.noreply.github.com>
2023-05-18 00:10:23 +0300

    # Домашнее задание 15.1 доработка

--- src/phone.py
+++ src/phone.py
@@ -3,11 +3,25 @@ from electronics_shop_project.src.item import Item
 
 class Phone(Item):
 
+
     def __init__(self,name: str, price: float, quantity: int, number_of_sim: int):
         # Вызываем метод базового класса
         super().__init__(name, price, quantity)
-        # Дополнительный код
         self.number_of_sim = number_of_sim
+
+
+    @property
+    def number_of_sim(self):
+        """Геттер метода number_of_sim"""
+        return self.__number_of_sim
+
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int):
+        """Сеттер метода number_of_sim"""
+        if value <= 0:
+            raise ValueError("Количество физических SIM-карт должно быть целым числом больше нуля")
+        self.__number_of_sim = value
 
 
     def __repr__(self):
